fa91e9d15d40a9ec693ddc541293909f8d5912d1
Jaka Munda <jaka.munda@siol.net>
2019-12-01 20:45:31 +0100

    koncano

--- domaca-naloga-01/lambda/eval.ml
+++ domaca-naloga-01/lambda/eval.ml
@@ -43,18 +43,48 @@ let rec eval_exp = function
       | S.RecLambda (f, x, e) as rec_f -> eval_exp (S.subst [(f, rec_f); (x, v)] e)
       | _ -> failwith "Function expected"
       end
+  | S.Pair (e1, e2) -> 
+      let v1 = eval_exp e1
+      and v2 = eval_exp e2
+      in S.Pair (v1, v2)
+  | S.Nil -> S.Nil
+  | S.Cons (e, es) ->
+      let v = eval_exp e
+      and vs = eval_exp es 
+      in S.Cons (v, vs)
+  | S.Fst (S.Pair (e1, e2)) -> 
+      let v1 = eval_exp e1
+      and v2 = eval_exp e2
+      in v1
+  | S.Snd (S.Pair (e1, e2)) -> 
+      let v1 = eval_exp e1
+      and v2 = eval_exp e2 
+      in v2
+  | S.Match (e, e1, x, xs, e2) ->
+      let v = eval_exp e
+      in
+      begin match v with
+      | S.Nil -> eval_exp e1
+      | S.Cons (v0, vs) -> 
+      let sub = S.subst [(x, v0); (xs, vs)] e2
+      in eval_exp sub
+      | _ -> failwith "List expected"
+      end
 and eval_int e =
   match eval_exp e with
   | S.Int n -> n
   | _ -> failwith "Integer expected"
 
-let is_value = function
-  | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> true
+let rec is_value = function
+  | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ | S.Nil -> true
   | S.Var _ | S.Plus _ | S.Minus _ | S.Times _ | S.Equal _ | S.Less _ | S.Greater _
-  | S.IfThenElse _ | S.Apply _ -> false
+  | S.IfThenElse _ | S.Apply _ | S.Fst _ | S.Snd _ | S.Match _ -> false
+  | S.Pair (e1, e2) -> (is_value e1) && (is_value e2)
+  | S.Cons (e, es) -> (is_value e) && (is_value es)
+
 
 let rec step = function
-  | S.Var _ | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> failwith "Expected a non-terminal expression"
+  | S.Var _ | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ | S.Nil -> failwith "Expected a non-terminal expression"
   | S.Plus (S.Int n1, S.Int n2) -> S.Int (n1 + n2)
   | S.Plus (S.Int n1, e2) -> S.Plus (S.Int n1, step e2)
   | S.Plus (e1, e2) -> S.Plus (step e1, e2)
@@ -79,6 +109,25 @@ let rec step = function
   | S.Apply (S.RecLambda (f, x, e) as rec_f, v) when is_value v -> S.subst [(f, rec_f); (x, v)] e
   | S.Apply ((S.Lambda _ | S.RecLambda _) as f, e) -> S.Apply (f, step e)
   | S.Apply (e1, e2) -> S.Apply (step e1, e2)
+  | S.Fst (S.Pair (v, _)) when is_value v -> v
+  | S.Fst (S.Pair (e, v)) -> S.Fst (S.Pair (step e, v))
+  | S.Fst _ -> failwith "Pair expected"
+  | S.Snd (S.Pair (_, v)) when is_value v-> v
+  | S.Snd (S.Pair (v, e)) -> S.Fst (S.Pair (v, step e))
+  | S.Snd _ -> failwith "Pair expected"
+  | S.Pair (v1, v2) when (is_value v1 && is_value v2) -> failwith "Expected a non-terminal expression"
+  | S.Pair (v, e) when is_value v -> S.Pair (v, step e)
+  | S.Pair (e, v) -> S.Pair (step e, v)
+  | S.Cons (v1, v2) when (is_value v1 && is_value v2) -> failwith "Expected a non-terminal expression"
+  | S.Cons (v, e) when is_value v -> S.Cons (v, step e)
+  | S.Cons (e, v) -> S.Cons (step e, v)
+  | S.Match (e, e1, x, xs, e2) when is_value e -> 
+    begin match e with
+    | S.Nil -> e1
+    | S.Cons (v, vs) -> S.subst [(x, v); (xs, vs)] e2
+    | _ -> failwith "List expected"
+end
+  | S.Match (e, e1, x, xs, e2) -> S.Match (step e, e1, x, xs, e2)
 
 let big_step e =
   let v = eval_exp e in
--- domaca-naloga-01/lambda/lazyEval.ml
+++ domaca-naloga-01/lambda/lazyEval.ml
@@ -36,25 +36,36 @@ let rec eval_exp = function
       end
   | S.Apply (e1, e2) ->
       let f = eval_exp e1
-      and v = eval_exp e2
       in
       begin match f with
-      | S.Lambda (x, e) -> eval_exp (S.subst [(x, v)] e)
-      | S.RecLambda (f, x, e) as rec_f -> eval_exp (S.subst [(f, rec_f); (x, v)] e)
+      | S.Lambda (x, e) -> eval_exp (S.subst [(x, e2)] e)
+      | S.RecLambda (f, x, e) as rec_f -> eval_exp (S.subst [(f, rec_f); (x, e2)] e)
       | _ -> failwith "Function expected"
       end
+  | S.Pair (e1, e2) -> S.Pair (e1, e2)
+  | S.Nil -> S.Nil
+  | S.Cons (e, es) -> S.Cons (e, es)
+  | S.Fst (S.Pair (e1, e2)) -> eval_exp e1
+  | S.Snd (S.Pair (e1, e2)) -> eval_exp e2
+  | S.Match (e, e1, x, xs, e2) ->
+      let v = eval_exp e in
+      begin match v with
+      | S.Nil -> eval_exp e1
+      | S.Cons (e0, es) -> eval_exp (S.subst [(x, e0); (xs, es)] e2)
+      | _ -> failwith "List expected"
+    end
 and eval_int e =
   match eval_exp e with
   | S.Int n -> n
   | _ -> failwith "Integer expected"
 
-let is_value = function
-  | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> true
+let rec is_value = function
+  | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ | S.Nil | S.Pair _ | S.Cons _ -> true
   | S.Var _ | S.Plus _ | S.Minus _ | S.Times _ | S.Equal _ | S.Less _ | S.Greater _
-  | S.IfThenElse _ | S.Apply _ -> false
+  | S.IfThenElse _ | S.Apply _ | S.Fst _ | S.Snd _ | S.Match _ -> false
 
 let rec step = function
-  | S.Var _ | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> failwith "Expected a non-terminal expression"
+  | S.Var _ | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ | S.Nil | S.Pair _ | S.Cons _ -> failwith "Expected a non-terminal expression"
   | S.Plus (S.Int n1, S.Int n2) -> S.Int (n1 + n2)
   | S.Plus (S.Int n1, e2) -> S.Plus (S.Int n1, step e2)
   | S.Plus (e1, e2) -> S.Plus (step e1, e2)
@@ -79,6 +90,16 @@ let rec step = function
   | S.Apply (S.RecLambda (f, x, e) as rec_f, v) when is_value v -> S.subst [(f, rec_f); (x, v)] e
   | S.Apply ((S.Lambda _ | S.RecLambda _) as f, e) -> S.Apply (f, step e)
   | S.Apply (e1, e2) -> S.Apply (step e1, e2)
+  | S.Fst (S.Pair (v, _)) -> v  
+  | S.Fst v when is_value v -> failwith "Pair expected"
+  | S.Fst e -> S.Fst (step e)
+  | S.Snd (S.Pair (_, v)) -> v  
+  | S.Snd v when is_value v -> failwith "Pair expected"
+  | S.Snd e -> S.Fst (step e)
+  | S.Match (S.Nil, e1, x, xs, e2) -> e1
+  | S.Match (S.Cons (y, ys), e1, x, xs, e2) -> S.subst [(x, y); (xs, ys)] e2
+  | S.Match (e, e1, x, xs, e2) when is_value e -> failwith "List expected"
+  | S.Match (e, e1, x, xs, e2) -> S.Match (step e, e1, x, xs, e2)
 
 let big_step e =
   let v = eval_exp e in
